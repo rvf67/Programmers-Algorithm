0451c77c2aabf3404e5260bf2b206a3c9308fa7c
rvf67 <62585538+rvf67@users.noreply.github.com>
2024-02-07 18:00:52 +0900

    Create FileDelete.cpp
    바탕화면 정리문제 클리어(C++)

---
+++ FileDelete.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//https://school.programmers.co.kr/learn/courses/30/lessons/161990
+//프로그래머스 바탕화면 정리문제
+
+//아이디어
+//단순 크기비교
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+    int minX = 50;
+    int minY = 50;
+    int maxX = 0;
+    int maxY = 0;
+    for (int i = 0; i < wallpaper.size(); i++) {
+        for (int j = 0; j < wallpaper[0].size(); j++) {
+            if (wallpaper[i][j] == '#') {
+                minX = min(minX, i);
+                minY = min(minY, j);
+                maxX = max(maxX, i+1);
+                maxY = max(maxY, j+1);
+            }
+        }
+    }
+    answer = { minX,minY,maxX,maxY };
+    return answer;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
